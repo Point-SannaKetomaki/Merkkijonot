9aad512f5e794b00958db1c43966204d468278bb
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2018-11-27 17:00:57 +0200

    Koodia kirjoitettu

--- Merkkijonot_ohj-jatko_pv03_esim01/Merkkijonot_ohj-jatko_pv03_esim01/Program.cs
+++ Merkkijonot_ohj-jatko_pv03_esim01/Merkkijonot_ohj-jatko_pv03_esim01/Program.cs
@@ -10,6 +10,25 @@ namespace Merkkijonot_ohj_jatko_pv03_esim01
     {
         static void Main(string[] args)
         {
+            //huono tapa, raskas roskien keräyksen mielessä.
+            //string s = "ABC";
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    s = s + "!";
+            //}
+            //Console.WriteLine(s);
+            //Console.ReadLine();
+
+
+            //parempi tapa, ohjelma ajaa käskyn huomattavasti nopeammin.
+            string s = "";
+            StringBuilder sb = new StringBuilder("ABC");
+            for (int i = 0; i < 100000; i++)
+            {
+                sb.Append("!");
+            }
+            Console.WriteLine(s = sb.ToString());
+            Console.ReadLine();
         }
     }
 }
